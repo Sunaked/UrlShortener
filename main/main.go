3ed908bee30a28b47e492d634269b0e77dff1165
Sunaked <syrgulev-aa@mail.com>
2022-02-04 20:03:37 +0300

    Added yaml flag that parses given file and append them into the short-url long-url list

--- main/main.go
+++ main/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	urlshort "example.com/URLShortener"
 	// "github.com/gophercises/urlshort"
@@ -10,7 +12,10 @@ import (
 
 func main() {
 	mux := defaultMux()
+	pathToYAMLFile := flag.String("yml", "", "parses yaml file and appends http.Handler to work with them also")
 
+	_ = pathToYAMLFile
+	flag.Parse()
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
@@ -19,18 +24,17 @@ func main() {
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
+
+	if *pathToYAMLFile != "" {
+		file, err := os.ReadFile(*pathToYAMLFile)
+		mapHandler, err = urlshort.YAMLHandler([]byte(file), mapHandler)
+		if err != nil {
+			panic(err)
+		}
 	}
+
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
