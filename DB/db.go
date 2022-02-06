9576af4aee20f0fba6e221b7a551cc92a0aa34f1
Sunaked <syrgulev-aa@mail.com>
2022-02-06 16:49:30 +0300

    left only functions

--- DB/db.go
+++ DB/db.go
@@ -1,4 +1,4 @@
-package main
+package shortener
 
 import (
 	"database/sql"
@@ -8,69 +8,154 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func main() { //
-	URLsShort := map[string]string{"/vk": "https://vk.com/im?peers=194134042", "/stock": "https://ru.tradingview.com/chart/CPNxT7rI/?symbol=NYSE%3ANEE", "/timetable": "https://webservices.mirea.ru/upload/iblock/e38/2cu5la01h6zeg1la9upv0o4fphvt3izu/ИИИ_2 курс_21-22_весна.xlsx"}
+// URLShortenerDB that provides
+// asdasd (just a text plug)
+type URLShortenerDB struct {
+	DataBase *sql.DB
+}
 
-	_ = URLsShort
-	connStr := "user=alex password=222"
-	fmt.Println("Hello SQL")
-	db, err := sql.Open("postgres", connStr)
+//NewURLShortenerDB do something
+func NewURLShortenerDB(connStr string) *URLShortenerDB {
+	dataBase, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err)
 	}
-
-	err = db.Ping()
+	shortener := &URLShortenerDB{DataBase: dataBase}
+	err = shortener.CreateTable()
 	if err != nil {
-		fmt.Println("DataBase is not responding, err:", err)
+		panic(err)
 	}
+	return shortener
+}
 
+//CreateTable creates a table with Path, Url values set to nil values
+func (db *URLShortenerDB) CreateTable() error { //
+
+	//Create TABLE Query WITH `Path` AND `Url` VALUES
 	createTableQuery := `
 	DROP TABLE Shortener;
 	CREATE TABLE Shortener (
 		Path varchar(255),
 		Url varchar(255)
 	);`
-	_, err = db.Exec(createTableQuery)
+	// Create table
+	_, err := db.DataBase.Exec(createTableQuery)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
-	q := `
-	SELECT * FROM Shortener
-	`
-	rows, err := db.Query(q)
-	defer rows.Close()
-	col, err := rows.Columns()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fmt.Println(col)
+	return nil
+}
 
+//InsertLongShortData insert given map[string]string data into the database
+func (db *URLShortenerDB) InsertLongShortData(data map[string]string) error {
 	// Inserting data into the database:
-	stmt, err := db.Prepare(`INSERT INTO Shortener(Path, Url) VALUES( $1, $2 )`)
+	stmt, err := db.DataBase.Prepare(`INSERT INTO Shortener(Path, Url) VALUES( $1, $2 )`)
 	if err != nil { //
-		panic(err)
+		return err
 	}
-	for key, value := range URLsShort { //
+	for key, value := range data { //
 		if _, err := stmt.Exec(key, value); err != nil {
-			fmt.Println(err)
+			return err
 		}
 	}
+	return nil
+}
 
-	q = `
+func (db *URLShortenerDB) String() {
+	q := `
 	SELECT Path, Url FROM Shortener
 	`
-	rows, err = db.Query(q)
+	//Getting these values
+	rows, err := db.DataBase.Query(q)
+
+	//Printing that values
 	defer rows.Close()
+
 	for rows.Next() {
 		var Path string
 		var URL string
-		err = rows.Scan(&Path, &URL)
-		if err != nil {
-			break
+		for rows.Next() {
+			err = rows.Scan(&Path, &URL)
+			if err != nil {
+				break
+			}
+			fmt.Print(Path, "\t", URL, "\n")
 		}
-		fmt.Println(Path, URL)
 	}
 
 }
+
+//GetLongURL gives long URL after receiving a short one
+func (db *URLShortenerDB) GetLongURL(url string) string {
+	//Getting Query
+	rows, err := db.DataBase.Query("SELECT Url FROM Shortener WHERE Path = $1", url)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	//Printing that values
+
+	var result string
+	for rows.Next() {
+		err = rows.Scan(&result)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	return result
+}
+
+// func main() { //
+// URLsShort := map[string]string{"/vk": "https://vk.com/im?peers=194134042", "/stock": "https://ru.tradingview.com/chart/CPNxT7rI/?symbol=NYSE%3ANEE", "/timetable": "https://webservices.mirea.ru/upload/iblock/e38/2cu5la01h6zeg1la9upv0o4fphvt3izu/ИИИ_2 курс_21-22_весна.xlsx"}
+
+// connStr := "user=alex password=222"
+
+////////////New not parralel version
+// shortener := newURLShortenerDB(connStr)
+// err := shortener.CreateTable()
+// if err != nil {
+// 	panic(err)
+// }
+// err = shortener.insertLongShortData(URLsShort)
+// if err != nil {
+// 	panic(err)
+// }
+// // shortener.String()
+// url := shortener.getLongURL("/vk")
+// fmt.Println(url)
+
+///////////////Last version////////////////////////////////
+
+// ///Connect to database with `connStr` parameters
+// db, err := sql.Open("postgres", connStr)
+// if err != nil {
+// 	panic(err)
+// }
+
+// //Ping the database if it is available, else "DataBase is not responding, err", err
+// err = db.Ping()
+// if err != nil {
+// 	fmt.Println("DataBase is not responding, err:", err)
+// }
+
+// // Select Query to get all Path, Url values
+// q := `
+// SELECT Path, Url FROM Shortener
+// `
+// //Getting these values
+// rows, err := db.Query(q)
+// defer rows.Close()
+// //Printing that values
+// for rows.Next() {
+// 	var Path string
+// 	var URL string
+// 	err = rows.Scan(&Path, &URL)
+// 	if err != nil {
+// 		break
+// 	}
+// 	fmt.Println(Path, URL)
+// }
+
+// }
